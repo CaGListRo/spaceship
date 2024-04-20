52f9d5c17116b320f59c5dfb4214b06f7f7e4487
CaGListRo <paco.nyns@gmail.com>
2024-04-20 19:44:01 +0200

    WIP:Tested a bubble sort for sorting a new name into the highscore list. Started to create logo for the title screen.

--- test.py
+++ test.py
@@ -8,7 +8,20 @@ with open("highscore_list.txt", "r", encoding="utf-8") as file:
         word = line.split(" ")
         highscore_list.append([word[0], int(word[1])])
 
-print(lines)
+
 print(highscore_list)
 
+highscore_list.append(['Paco', 55000] )
+print(highscore_list)
 
+while True:
+    bubbled = False
+    for i in range(len(highscore_list)-1):  
+        if highscore_list[i][1] < highscore_list[i+1][1]:
+            highscore_list[i+1], highscore_list[i] = highscore_list[i], highscore_list[i+1]
+            bubbled = True
+            
+    if not bubbled:
+        break
+
+print(highscore_list)
