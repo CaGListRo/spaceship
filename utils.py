11ca1ade910e2066564215a0f4331e6e153b80e1
CaGListRo <paco.nyns@gmail.com>
2024-11-11 16:40:09 +0100

    Added the last missing docstrings too the utils.py.

--- utils.py
+++ utils.py
@@ -106,6 +106,13 @@ def sort_and_write_highscores(highscores_list: list[str], name: str, score: int)
 
 class Animation:
     def __init__(self, image_list: list[pg.Surface], animation_duration: int | float, loop: bool = True) -> None:
+        """
+        Initializes an animation object.
+        Args:
+        image_list (list[pg.Surface]): The list of images to animate.
+        animation_duration (int | float): The duration of the animation in seconds.
+        loop (bool): Whether the animation should loop or not. Defaults to True.
+        """
         self.img_list: list[pg.Surface] = list(image_list)
         self.anim_dur: int | float = animation_duration
         self.loop: bool = loop
@@ -115,9 +122,19 @@ class Animation:
         self.img_timer: int | float = 0
 
     def copy(self) -> Animation_object:
+        """
+        Creates a copy of the animation object and returns it.
+        Returns:
+        Animation_object: The copied animation object.
+        """
         return Animation(self.img_list, self.anim_dur, self.loop)
 
     def update(self, dt: float) -> None:
+        """
+        Updates the animation.
+        Args:
+        dt (float): The time elapsed since the last frame.
+        """
         self.img_timer += dt
         if self.img_timer >= self.img_duration and not self.done:
             self.current_frame += 1
@@ -129,6 +146,11 @@ class Animation:
                     self.done = True
 
     def get_img(self) -> pg.Surface | None:
+        """
+        Gets the current image of the animation.
+        Returns:
+        pg.Surface | None: The current image of the animation, or None if the animation is done
+        """
         if 0 <= self.current_frame < len(self.img_list):
             return self.img_list[self.current_frame]
         else:
@@ -139,6 +161,11 @@ class Helpsite:
     TRANSPARENT_BACKGROUND: Final[tuple[int]] = (0, 0, 0, 0)
 
     def __init__(self, game: Game) -> None:
+        """
+        Initializes the help site.
+        Args:
+        game (Game): The game object.
+        """
         self.game: Game = game
         self.font = pg.font.SysFont("comicsans", 32)
         self.surface = pg.Surface((1600, 900), pg.SRCALPHA)
@@ -156,9 +183,11 @@ class Helpsite:
         self.draw_upgrades()
 
     def generate_background_numbers(self) -> None:
+        """ Generate a list of 11 numbers from 0 to 6 for the upgrade background colors. """
         self.background_numbers = [randint(0, 6) for _ in range(11)]
 
-    def draw_help_text(self) -> None:       
+    def draw_help_text(self) -> None:
+        """ Opens the help_text.txt and draws its content on the help site surface. """
         with open("help_text.txt", "r", encoding="utf-8") as file:
             text = file.read()
             lines = text.splitlines()
@@ -167,6 +196,10 @@ class Helpsite:
                 self.surface.blit(text, (50, 50 + i * 50))
 
     def draw_upgrades(self) -> None:
+        """
+        Draws the upgrade background and the upgrade image on the help site surface.
+        And places an collision rectangle over each upgrade for the help text display.
+        """
         self.upgrade_rect_list = []
         for i, upgrade in enumerate(self.game.assets["upgrade/image"]):
             self.surface.blit(self.game.assets["upgrade/background"][self.background_numbers[i]], (50 + i * 130, 420))
@@ -174,6 +207,12 @@ class Helpsite:
             self.upgrade_rect_list.append(pg.Rect(50 + i * 130, 420, self.rect_width_height, self.rect_width_height))
 
     def check_rect_collision(self) -> int:
+        """
+        Checks if the mouse is over any of the upgrade rectangles.
+        Returns the index of the rectangle if the mouse is over it, otherwise returns -1.
+        Returns:
+        int: The index of the rectangle if the mouse is over it, otherwise -1.
+        """
         self.mouse_pos = pg.mouse.get_pos()
         for i, rect in enumerate(self.upgrade_rect_list):
             if rect.collidepoint(self.mouse_pos):
@@ -181,14 +220,20 @@ class Helpsite:
         return -1
     
     def handle_upgrade_texts(self) -> None:
+        """
+        Gets the upgrade text number from the check_rect_collision-method,
+        loads it from self.text_list, renders it and blits the result to self.text_surf. 
+        """
         self.text_number = self.check_rect_collision()
-        self.text_surf.fill((111, 111, 111))
+        self.text_surf.fill(self.TRANSPARENT_BACKGROUND)
         for i, line in enumerate(self.text_list[self.text_number]):
             line_to_blit = self.font.render(line, True, (247, 247, 247))
             self.text_surf.blit(line_to_blit, (0, 0 + i * 50))
 
-
-    def load_upgrade_texts(self) -> None:        
+    def load_upgrade_texts(self) -> None:
+        """
+        Loads the upgrade text from the upgrade_texts.txt file, stplits it in lines 
+        and stores it in the two dimensional self.text_list. """
         with open("upgrade_texts.txt", "r", encoding="utf-8") as file:
             lines = file.readlines()
             help_list = []
@@ -202,12 +247,18 @@ class Helpsite:
             self.text_list.append(help_list)
 
     def update(self) -> None:
-        self.surface.fill((111, 111, 111))
+        """ Updates the help site. """
+        self.surface.fill(self.TRANSPARENT_BACKGROUND)
         self.handle_upgrade_texts()
         self.draw_help_text()
         self.draw_upgrades()
 
-    def render(self, surf) -> None:  
+    def render(self, surf: pg.Surface) -> None:
+        """
+        Renders the help site to the given surface.
+        Args:
+        surf: The surface to render the help site to.
+        """ 
         if self.text_number != -1:
             if self.mouse_pos[0] + self.text_surf.get_width() > stgs.MAIN_WINDOW_RESOLUTION[0]:
                 
